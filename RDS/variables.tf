f638e5f64faddf665c3a40bc40572ad4321454c0
Lincoln Roy <roylincoln002@gmail.com>
2023-07-09 15:50:33 -0400

    created RDS

---
+++ RDS/variables.tf
@@ -0,0 +1,11 @@
+# RDS variables
+variable "project_name" {}
+variable "environment" {}
+variable "private_data_subnet_az1_id" {}
+variable "private_data_subnet_az2_id" {}
+variable "db_snapshot_identifier" {}
+variable "db_instance_class" {}
+variable "availability_zone_1" {}
+variable "db_instance_identifier" {}
+variable "multi_az_deployment" {}
+variable "database_security_group_id" {}
